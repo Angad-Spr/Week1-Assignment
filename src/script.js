62ee2fcafd66a96fd0349e8bff03cef4a2c50c73
Gunangad Pal Singh Narula <gunangad.pal@sprinklr.com>
2022-05-20 23:55:39 +0530

    Update Image window on selection

--- src/script.js
+++ src/script.js
@@ -18,8 +18,28 @@ console.log(data);
 const addDataToPage = () => {
   let ulData = "";
   data.forEach((dataInstance) => (ulData += markup(dataInstance)));
-  const list = document.getElementsByTagName("ul")[0];
+  const list = document.querySelector("ul");
   list.innerHTML = ulData;
 };
 
+const selectPreview = (index = 0) => {
+  const oldSelection = document.querySelector(".selected");
+  if (oldSelection !== null) {
+    oldSelection.classList.remove("selected");
+  }
+  const list = document.querySelector("ul");
+  if (index < list.children.length) {
+    list.children[index].classList.add("selected");
+    const imgLink = list.children[index]
+      .querySelector("img")
+      .getAttribute("src");
+    document.querySelector(".image-container img").setAttribute("src", imgLink);
+  } else {
+    console.error("Index more than list");
+  }
+};
+
 addDataToPage();
+document
+  .querySelectorAll("li")
+  .addEventListener("click", selectPreview(1), false);
