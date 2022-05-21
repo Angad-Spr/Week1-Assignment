17a5d2a8d623a0b4c96a1d8409c9d511ac3b5714
Gunangad Pal Singh Narula <gunangad.pal@sprinklr.com>
2022-05-21 20:23:14 +0530

    Add Keyboard Switching Update index resolution for selectPreview()

--- src/script.js
+++ src/script.js
@@ -13,47 +13,60 @@ const markup = (dataInstance) => {
 `;
 };
 
-// const data = require("./data.json");
-console.log(data);
 const addDataToPage = () => {
   let ulData = "";
   data.forEach((dataInstance) => (ulData += markup(dataInstance)));
   const list = document.querySelector("ul");
   list.innerHTML = ulData;
+  selectPreview();
 };
 
 const selectPreview = (index = 0) => {
+  const list = document.querySelector("ul");
+
+  // Bad Index
+  if (index < 0 || index >= list.children.length) {
+    return;
+  }
+
+  // Remove Old Selection
   const oldSelection = document.querySelector(".selected");
   if (oldSelection !== null) {
     oldSelection.classList.remove("selected");
   }
-  const list = document.querySelector("ul");
-  if (index < list.children.length) {
-    list.children[index].classList.add("selected");
-    const imgLink = list.children[index]
-      .querySelector("img")
-      .getAttribute("src");
-    document.querySelector(".image-container img").setAttribute("src", imgLink);
-  } else {
-    console.error("Index more than list");
-  }
+
+  // Select New Image
+  list.children[index].classList.add("selected");
+  const imgLink = list.children[index].querySelector("img").getAttribute("src");
+  document.querySelector(".image-container img").setAttribute("src", imgLink);
 };
 
-const getIndex = (listElement) => {
+const getIndex = (listElement = document.querySelector(".selected")) => {
   return Array.from(listElement.parentNode.children).indexOf(listElement);
 };
 
 const makeClickable = () => {
-  //   const list = document.querySelector("ul");
-  //   {
-  //     for(i in)list
-  //   }
+  Array.from(document.querySelector("ul").children).forEach((listElement) =>
+    listElement.addEventListener("click", function () {
+      selectPreview(getIndex(this));
+    })
+  );
+};
+
+const keyboardSwitching = () => {
+  document.addEventListener("keydown", function (event) {
+    const key = event.key;
+    switch (key) {
+      case "ArrowDown":
+        selectPreview(getIndex() + 1);
+        break;
+      case "ArrowUp":
+        selectPreview(getIndex() - 1);
+        break;
+    }
+  });
 };
 
 addDataToPage();
-Array.from(document.querySelector("ul").children).forEach((listElement) =>
-  //   (listElement) => console.log(listElement)
-  listElement.addEventListener("click", function () {
-    selectPreview(getIndex(this));
-  })
-);
+makeClickable();
+keyboardSwitching();
